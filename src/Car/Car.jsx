d1fe2c36a9dd8a010771275b0e8d8736d6be14aa
HuziMuzi <laif17860@mail.ru>
2022-03-17 11:33:07 +0300

    change components

---
+++ src/Car/Car.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+
+		
+
+
+export default props => (
+	<div>
+		<h3>Car name: {props.name}</h3>
+		<p>Year: <strong>{props.year}</strong></p>
+	</div>
+)
